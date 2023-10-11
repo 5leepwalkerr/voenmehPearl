e01e723b5d06971a34249e00404487372cc4895c
awesomemax <cvb.cvb.2017.cvb@gmail.com>
2023-10-11 23:28:09 +0300

    init commit

---
+++ src/test/java/com/voenmeh/voenmehpearl/VoenmehPearlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voenmeh.voenmehpearl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoenmehPearlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
