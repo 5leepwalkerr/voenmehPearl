0f666585e5c201cd3fb3d91dbf6a3a0e30ccdc30
awesomemax <cvb.cvb.2017.cvb@gmail.com>
2023-10-13 13:40:24 +0300

    add voenmehFile model

--- src/main/java/com/voenmeh/voenmehpearl/model/VoenmehFile.java
+++ src/main/java/com/voenmeh/voenmehpearl/model/VoenmehFile.java
@@ -1,4 +1,24 @@
 package com.voenmeh.voenmehpearl.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.File;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "voenmehFile")
 public class VoenmehFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "fileId",nullable = false)
+    private Long fileId;
+
+    @Column(name = "voenmehFile",nullable = true)
+    private File voenmehFile;
+
 }
