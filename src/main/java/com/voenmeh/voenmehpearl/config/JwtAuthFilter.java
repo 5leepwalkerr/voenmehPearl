75b5796611f133240e2b312e0e51ce7dc2c83a0c
awesomemax <cvb.cvb.2017.cvb@gmail.com>
2023-10-22 00:49:44 +0300

    create jwtFilter for auth

---
+++ src/main/java/com/voenmeh/voenmehpearl/config/JwtAuthFilter.java
@@ -0,0 +1,48 @@
+package com.voenmeh.voenmehpearl.config;
+
+import com.voenmeh.voenmehpearl.service.JwtService;
+import com.voenmeh.voenmehpearl.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private UserService userService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authHeader.isEmpty() || !authHeader.startsWith("Baerer ")){
+            filterChain.doFilter(request,response);
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUserName(jwt);
+        if (!userEmail.isEmpty() && SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails = userService.userDetailsService()
+                    .loadUserByUsername(userEmail);
+            if (jwtService.isTokenValid(jwt,userDetails)){
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                context.setAuthentication(authToken);
+                SecurityContextHolder.setContext(context);
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+}
---
+++ src/main/java/com/voenmeh/voenmehpearl/controller/AuthController.java
@@ -0,0 +1,2 @@
+package com.voenmeh.voenmehpearl.controller;public class AuthController {
+}
